72743b6396783c55f6726c957303474d279bbcb1
jimmabelle <jimmabelle@users.noreply.github.com>
2018-04-16 09:49:07 +0200

    starting position and zoom added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamltbWFiZWxsZSIsImEiOiJjamZ5MTBjeHAyNnYyMndxbjAyOTI0Y24yIn0.mvyT35xOV0oEtad34I0Vgg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [17.675409, 64.964875],
+    zoom: 1
 });
